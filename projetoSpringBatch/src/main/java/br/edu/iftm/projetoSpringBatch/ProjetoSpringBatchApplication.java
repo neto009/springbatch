91dfda49ca4206a5cb92c61eada5696ad4a1fb87
Neto <solange.neto@mitraecp.com>
2022-12-06 23:19:53 -0300

    ajustes

---
+++ projetoSpringBatch/src/main/java/br/edu/iftm/projetoSpringBatch/ProjetoSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.projetoSpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBatchApplication.class, args);
+	}
+
+}
---
+++ projetoSpringBatch/src/test/java/br/edu/iftm/projetoSpringBatch/ProjetoSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.projetoSpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
